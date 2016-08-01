11f3cb68a45bb282aae6a6ef8e35cfd4b3ba0267
Jordi Vimod <jordi@vimod.net>
2016-08-01 16:23:37 +0200

    auth

--- models/user.js
+++ models/user.js
@@ -1,51 +1,79 @@
-var bcrypt= require('bcrypt');
-var _= require('underscore');
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
 
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true,
-				validate: {
+	var user= sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
 				isEmail: true
-				}
-			},
-			salt:{
-				type: DataTypes.STRING
-			},
-			passord_hash:{
-				type:DataTypes.STRING
-			},
-			password: {
+			}
+		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
 			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
-				},
-			set: function(value){
-				var salt= bcrypt.genSaltSync(10);
-				var hashedPassword=bcrypt.hashSync(value,salt);
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
 				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
-				this.setDataValue('passord_hash', hashedPassword);
-				}
+				this.setDataValue('password_hash', hashedPassword);
 			}
-		},{
-	
-			hooks: {
-				beforeValidate: function(user, options) {
-					if (typeof user.email === "string") {
+		}
+	}, {
+
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === "string") {
 					user.email = user.email.toLowerCase();
-					}
-				}
-			}, instanceMethods:{
-				toPublicJSON:function(){
-					var json= this.toJSON();
-					return _.pick(json,'id','email','createdAt','updatedAt');
 				}
 			}
-			
-		});
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise( function(resolve, reject) {
+							if (typeof body.password !== 'string' || typeof body.email !== 'string') {
+								return reject();
+							}
+							//find one
+							user.findOne({
+								where: {
+									email: body.email
+								}
+							}).then(function(user) {
+								if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+									return reject();
+								}
+
+								resolve(user);
+
+							}, function(e) {
+								reject();
+							});
+
+				});
+			}
+
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			}
+		}
+
+	});
+return user;
 };
