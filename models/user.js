0d322c40c5ce7c2be9e329d952709c8b4d49bafb
Jordi Vimod <jordi@vimod.net>
2016-08-02 10:47:38 +0200

    token

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
-
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
-	var user= sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -27,53 +28,69 @@ module.exports = function(sequelize, DataTypes) {
 			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
+
 				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
-
 		hooks: {
 			beforeValidate: function(user, options) {
-				if (typeof user.email === "string") {
+				// user.email
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
 		classMethods: {
 			authenticate: function(body) {
-				return new Promise( function(resolve, reject) {
-							if (typeof body.password !== 'string' || typeof body.email !== 'string') {
-								return reject();
-							}
-							//find one
-							user.findOne({
-								where: {
-									email: body.email
-								}
-							}).then(function(user) {
-								if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-									return reject();
-								}
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
 
-								resolve(user);
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
 
-							}, function(e) {
-								reject();
-							});
-
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
 				});
 			}
-
 		},
 		instanceMethods: {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function (type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
-
 	});
-return user;
+
+	return user;
 };
